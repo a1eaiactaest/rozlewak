68af879a9020166a90aa14184d86860faaad5025
Piotrek Rybiec <piotrrybiec28@gmail.com>
2023-04-28 14:56:24 +0200

    working version

--- src/main.cpp
+++ src/main.cpp
@@ -8,62 +8,54 @@ ezButton limitSwitch1(7);
 ezButton limitSwitch2(6);
 ezButton limitSwitch3(5);
 
-ezButton volButton1(8);
-ezButton volButton2(9);
-ezButton volButton3(10);
+ezButton volButton1(8); // sub vol
+ezButton volButton2(9); // start
+ezButton volButton3(10); // add vol
 
 #define LCD_SDA A4
 #define LCD_SCL A5
 
 #define LED_PIN 13
 
-#define BUTTON_DEBOUNCE_MS 100
+#define BUTTON_DEBOUNCE_MS 5
 
-struct BUTTONS {
-  ezButton button1, button2, button3;
-};
 
-ezButton get_struct_buttons(BUTTONS s, int i) {
-  switch(i) {
-    case 0: return s.button1;
-    case 1: return s.button2;
-    case 2: return s.button3;
-    default: return -1;
+ezButton limit_switches[] = {limitSwitch1, limitSwitch2, limitSwitch3};
+ezButton volume_buttons[] = {volButton1, volButton2, volButton3};
+
+void setup_buttons(ezButton buttons[]) {
+  for (int i=0; i < 3; i++){
+    String s = "DEBOUNCED BUTTON " + i;
+    Serial.println(s);
+    buttons[i].setDebounceTime(BUTTON_DEBOUNCE_MS);
   }
 }
 
-/* *** VOLUME BUTTONS *** */
-BUTTONS volume_buttons = {volButton1, volButton2, volButton3};
-
-BUTTONS limit_switches = {limitSwitch1, limitSwitch2, limitSwitch3};
-
-void setup_buttons(BUTTONS buttons) {
+void init_limit_switches_loop(){
   for (int i=0; i < 3; i++){
-    ezButton cbutton = get_struct_buttons(buttons, i);
-    cbutton.setDebounceTime(BUTTON_DEBOUNCE_MS);
+    limit_switches[i].loop();
   }
 }
 
-void init_buttons_loop(BUTTONS buttons) {
+void init_volume_buttons_loop(){
   for (int i=0; i < 3; i++){
-    Serial.println("INIT");
-    ezButton cbutton = get_struct_buttons(buttons, i);
-    cbutton.loop();
+    volume_buttons[i].loop();
   }
 }
 
 void handle_volume_buttons() {
+  for (int i=0; i < 3; i++){
+    if (limit_switches[i].isPressed()){
 
+    }
+  }
 }
 
 void handle_limit_switches() {
-  init_buttons_loop(limit_switches);
-
-  for (int i = 0; i < 3; i++){
-    ezButton cbutton = get_struct_buttons(limit_switches, i);
-    if (cbutton.isPressed()){
+  for (int i=0; i < 3; i++){
+    if (limit_switches[i].isPressed()){
       digitalWrite(LED_PIN, HIGH);
-    } else if (cbutton.isReleased()) {
+    } else if (limit_switches[i].isReleased()){
       digitalWrite(LED_PIN, LOW);
     }
   }
@@ -107,5 +99,6 @@ void setup() {
 }
 
 void loop() {
+  init_limit_switches_loop();
   handle_limit_switches();
 }
